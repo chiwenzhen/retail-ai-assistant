51612c76153f926a28b04f9a5851890028c4667f
Wenzhen CHI <your.email@example.com>
2025-11-27 10:07:30 +0800

    update

--- export_images/save_image_to_tar.sh
+++ export_images/save_image_to_tar.sh
@@ -1,15 +1,15 @@
 #保存python镜像
-docker save -o python-3.11-slim-bookworm.tar python:3.11-slim-bookworm
+docker save --platform=linux/amd64 -o python-3.11-slim-bookworm.tar python:3.11-slim-bookworm
 
 #保存redis镜像
-docker save -o redis-7-alpine.tar redis:7-alpine
+docker save --platform=linux/amd64 -o redis-7-alpine.tar redis:7-alpine
 
 #保存postgres镜像
-docker save -o postgres-16-bookworm.tar postgres:16-bookworm
+docker save --platform=linux/amd64 -o postgres-16-bookworm.tar postgres:16-bookworm
 
 #保存retail-ai-assistant-aegra镜像
-docker save -o retail-ai-assistant-aegra.tar retail-ai-assistant-aegra:latest
+docker save --platform=linux/amd64 -o retail-ai-assistant-aegra.tar retail-ai-assistant-aegra:latest
 
 #保存retail-ai-assistant-entry_server镜像
-docker save -o retail-ai-assistant-entry_server.tar retail-ai-assistant-entry_server:latest
+docker save --platform=linux/amd64 -o retail-ai-assistant-entry_server.tar retail-ai-assistant-entry_server:latest
 
